2fb577d9920946e039ff296def892b9a85fd04be
aypahyo <aypahyo@gmx.net>
2020-09-30 10:20:21 +0200

    moved demo to package johannes

---
+++ src/test/johannes/test.groovy
@@ -0,0 +1,17 @@
+package johannes.tests
+
+import org.junit.jupiter.api.*
+import johannes.devopsdus2020.*
+import johannes.dockerConnections.*
+
+class MyDockerTest {
+
+  @Test
+  void checksCredentials() {
+      def service = [checkCredentials: { true }] as DockerService
+      MyDocker uut = new MyDocker(service)
+      Boolean isOk = uut.checkCredentials()
+      Assertions.assertTrue(isOk)
+  }
+
+}
