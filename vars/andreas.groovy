71f67d9f70c4cb05f4240458630b036b46ac3faf
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-02 00:18:23 +0200

    simplify andreas.groovy

--- vars/andreas.groovy
+++ vars/andreas.groovy
@@ -4,8 +4,7 @@ def greet(ouput) {
     println "In vars/greeting.greet"
     println "${output}"
     
-    GreetingService service = new GreetingService()
-    Greeting myGreeting = new Greeting(service)
+    //GreetingService service = new GreetingService()
+    //Greeting myGreeting = new Greeting(service)
 
-    myGreeting.greet("${ouput}")
 }
