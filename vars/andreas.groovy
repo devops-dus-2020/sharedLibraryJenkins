79772ee9a8273416ee3be5c2cf31e3598a58e431
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-01 15:46:30 +0200

    simple vars/andreas.groovy

--- vars/andreas.groovy
+++ vars/andreas.groovy
@@ -1,4 +1,5 @@
 def greet(ouput) {
+    echo "WARNING:  ${output}"
     //andreas.GreetingService service = new andreas.GreetingService()
     //andreas.Greeting myGreeting = new andreas.Greeting(service)
 
