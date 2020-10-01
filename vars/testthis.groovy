69892e892561d724bf997655c985cd3714c5323a
Ray Subahri <raysubahri@posteo.de>
2020-10-01 17:06:49 +0200

    remove Caps in groovy file and move file to base vars folder

--- vars/testthis.groovy
+++ vars/testthis.groovy
@@ -1,10 +1,9 @@
-    import ray.connection.ExtServ
-    import ray.core.InterfaceServ
+import ray.connection.ExtServ
     
+
     ExtServ extserv = new ExtServ()
 
     def greeting(message) {
     
     echo (println extserv.call()) + "${message}"
-
     }
