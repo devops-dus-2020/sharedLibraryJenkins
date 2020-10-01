d678daecd41df9fa00af7d10892b070d775e2c7e
Ray Subahri <raysubahri@posteo.de>
2020-10-01 17:15:23 +0200

    remove println, echo only

--- vars/testthis.groovy
+++ vars/testthis.groovy
@@ -1,9 +1,8 @@
 import ray.connection.ExtServ
-    
 
     ExtServ extserv = new ExtServ()
 
     def greeting(message) {
     
-    echo (println extserv.call()) + "${message}"
+    echo extserv.call() + "${message}"
     }
