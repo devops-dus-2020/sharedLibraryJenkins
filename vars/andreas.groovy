5ce701a47c3892f5c70ce857c53cc858a5a55536
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-01 20:14:57 +0200

    add quotation marks around variable in vars/andreas.groovy

--- vars/andreas.groovy
+++ vars/andreas.groovy
@@ -2,10 +2,10 @@ import andreas.*
 
 def greet(ouput) {
     println "In vars/greeting.greet"
-    println ${output}
+    println "${output}"
     
     GreetingService service = new GreetingService()
     Greeting myGreeting = new Greeting(service)
 
-    myGreeting.greet(${ouput})
+    myGreeting.greet("${ouput}")
 }
