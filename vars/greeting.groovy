0f8adaaf9422c3b7d473f958cfe6c6f7d819e1fa
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-01 20:01:47 +0200

    add stdout to see I'm in vars/greeting.groovy

--- vars/greeting.groovy
+++ vars/greeting.groovy
@@ -1,6 +1,8 @@
 import andreas.*
 
 def greet(String ouput) {
+    println "In vars/greeting.greet"
+    
     GreetingService service = new GreetingService()
     Greeting myGreeting = new Greeting(service)
 
