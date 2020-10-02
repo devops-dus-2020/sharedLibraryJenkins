434846e31bc9c6f5b96b48f50a52ec1e48e6de81
Juli4ka30 <67759640+Juli4ka30@users.noreply.github.com>
2020-10-02 15:51:13 +0200

    Update hellolaunch.groovy

--- vars/hellolaunch.groovy
+++ vars/hellolaunch.groovy
@@ -3,7 +3,7 @@ import indu.core.*
 
 def greeting(message){
     HelloConnection hci = new HelloConnection()
-    HelloClass hc = new HelloClass(hci)
+    core.HelloClass hc = new core.HelloClass(hci)
 
     String str = hc.invokecall() + "${message}"
     echo str 
