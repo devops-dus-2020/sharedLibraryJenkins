60ec720a12c1bc717b82084a7bd38cbce4c42f71
melchma <67784962+melchma@users.noreply.github.com>
2020-10-02 11:40:04 +0200

    alles zurück auf echo

--- vars/werbung.groovy
+++ vars/werbung.groovy
@@ -1,11 +1,10 @@
 //TODO import statements
 import maik.devopsdusmaik.*
 import maik.core.*
-import groovy.util.logging.Log
 
-@Log
+
 def warning() {
     def s = new EchoService()
     Echo uut = new Echo(s)
-    log.info(uut.Name()) 
+    echo uut.Name()
 }
