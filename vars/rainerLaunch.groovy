3cf81478cfa1e6ed5e6eb322110f2fc3cef17bcd
RainerClassen <RainerClassen@example.com>
2020-10-02 15:38:29 +0200

    Noch eine Änderung

--- vars/rainerLaunch.groovy
+++ vars/rainerLaunch.groovy
@@ -1,10 +1,10 @@
 import rainerC.connection.RainerExtService
-import rainerC.core.Jeppie.*
+import rainerC.core.Jeppie
  
 
     def greeting(message) {
     RainerExtService myShout = new RainerExtService()
-    Jeppie bla = new Jeppie(myShout)
+    Jeppie ji = new Jeppie(myShout)
 
-    String hello = bla.shoutJeppie() + "${message}"
+    String hello = ji.shoutJeppie() + "${message}"
     echo hello
