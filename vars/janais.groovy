62d8bc435af77095d54b162650acd9f954f854f8
janasaba <67761428+janasaba@users.noreply.github.com>
2020-10-02 13:58:46 +0200

    Jenkins janais korrigiert

--- vars/janais.groovy
+++ vars/janais.groovy
@@ -5,7 +5,7 @@ import jana.dockerConnections.*
 def info(message){
     IFlaeche berechner = new FlaecheService()
      Flaecheberechner uut = new Flaecheberechner(berechner)
-    int flaeche = uut.FlaecheVonRechteck(a,b)
+    int flaeche = uut.FlaecheVonRechteck(1,1)
 
     println (flaeche)
 }
