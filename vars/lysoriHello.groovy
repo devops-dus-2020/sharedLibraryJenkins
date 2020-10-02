dfae36c468fc81cd6e58391413b608033fe7f2b7
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-02 10:24:47 +0200

    syntax fehler in vars file editiert

--- vars/lysoriHello.groovy
+++ vars/lysoriHello.groovy
@@ -12,8 +12,7 @@ def helloWorld(){
 def generell(message){
     def service = new HelloWorldService()
     HelloWorld h = new HelloWorld(service)
-    String result = "Bastis Generell-Generator: " 
-                        + h.generellGenerator(5) + " ${message}"
+    String result = "Bastis Generell-Generator: " + h.generellGenerator(5) + " ${message}"
 
     echo result
 }
