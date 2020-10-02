f968729dfe67abde1bd02c1b2a24ba7444775953
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-02 13:07:29 +0200

    add vars/testlogger

---
+++ vars/testlogger.groovy
@@ -0,0 +1,17 @@
+import andreas.*
+
+
+def log(variable) {
+    echo "${variable}"
+
+
+    //String output = "${variable}"
+    println "In vars/testlogger"
+
+    String output = 'Logging works!'
+
+    def bindingout = getBinding().out
+    JenkinsLogger logger = new JenkinsLogger(bindingout)
+    logger.print(output)
+    logger.prinln(output)
+}
