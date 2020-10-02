d6ef7e95eb33ec3f0b2ce9aa867569949f488ee7
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-02 14:14:53 +0200

    try config

--- vars/testlogger.groovy
+++ vars/testlogger.groovy
@@ -10,8 +10,8 @@ def log(variable) {
 
     String output = 'Logging works!'
 
-    bindingout = config['out']
     def binding = getBinding()
+    def bindingvars = binding.getVariables()
     binding.setProperty("out",new PrintWriter(stdout,true));
     def bindingout = binding.getProperty('out')
 
