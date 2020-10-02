25b717bc094aea45752f51833293c0a72bf8e9c5
lauragm93 <40059929+lauragm93@users.noreply.github.com>
2020-10-02 10:37:29 +0200

    add groovy test

---
+++ src/laura/devopsdus2020/IGeneratorService.groovy
@@ -0,0 +1,10 @@
+package laura.devopsdus2020
+
+import laura.details.*
+
+interface IGeneratorService {
+    
+     int generate(a) 
+     int calculate(x)
+     
+}
---
+++ src/laura/devopsdus2020/NumberGenerator.groovy
@@ -0,0 +1,26 @@
+package laura.devopsdus2020
+
+import laura.details.*
+
+class NumberGenerator {
+    
+    IGeneratorService gs;
+    int x = 0
+
+    public NumberGenerator(IGeneratorService gs)
+    {
+        this.gs = gs
+    }
+
+    public int generateNumber()
+    {   
+        int ret = gs.generate();
+        x += ret;
+        return ret;
+    }
+
+    public int calculateNumbers() {
+        return x;
+    }
+
+}
