e303e0e86a50a00bd2952973470b99fe085196b1
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-02 10:50:38 +0200

    fix myecho

--- vars/testgreet.groovy
+++ vars/testgreet.groovy
@@ -1,5 +1,10 @@
 import andreas.*
 
+def myecho(String msg)
+{
+    println msg
+}
+
 def greet(variable) {
     echo "${variable}"
 
@@ -11,8 +16,7 @@ def greet(variable) {
 
     GreetingService service = new GreetingService()
     Greeting myGreeting = new Greeting(service)
-    def closure = (String msg) -> {println msg}
-    myGreeting.redirectEcho("redirected", closure)
+    myGreeting.redirectEcho("redirected", myecho)
 
     myGreeting.greet(output)
 
