ad2a74e2eb77a6876002d32413e7a58d4654bd75
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-13 16:21:11 +0200

    pwd entfernt

--- vars/infincompile.groovy
+++ vars/infincompile.groovy
@@ -2,11 +2,9 @@ import infin.*
 import groovy.util.logging.*
 
 def compile() {
-    println "pwd".execute().text
     Closure logger = {String message -> println message}
     CompilerService service = new CompilerService(logger)
     Compiler myCompiler = new Compiler(service)
 
     logger(myCompiler.compile())
-    println "pwd".execute().text
 }
