c096284a66d0fe82fb3ac6ddadea8c61bf674dce
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-13 20:57:57 +0200

    pompath in vars file editiert

--- vars/infincompile.groovy
+++ vars/infincompile.groovy
@@ -10,6 +10,6 @@ def compile(pompath) {
 }
 
 //pompath bitte an deinen Jenkinsfile anpassen
-pompath = "./pom.xml"
+pompath = "/var/jenkins_home/workspace/BBROW/pom.xml"
 Map config = [flag:" -f ", pomfile: pompath]
 compile(config)
