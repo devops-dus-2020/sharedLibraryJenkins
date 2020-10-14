67cbac49a904479197dec90da0bb24c5b4f3da8e
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-14 15:36:17 +0200

    add global fields in vars file test

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -2,16 +2,16 @@ import de.devopsdus2020.maven.*
 import de.devopsdus2020.external.*
 import groovy.util.logging.*
 
-// def compile() {
-//     Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
-//     compile(config)
-// }
+@Field final Closure logger = {String message -> println message}
+@Field final MavenService service = new MavenService(logger)
+@Field final Maven myMaven = new Maven(service)
+@Field final Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
 
 def compile() {
-    Closure logger = {String message -> println message}
-    MavenService service = new MavenService(logger)
-    Maven myMaven = new Maven(service)
-    Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
+    //Closure logger = {String message -> println message}
+    //MavenService service = new MavenService(logger)
+    //Maven myMaven = new Maven(service)
+    //Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
     
     logger(myMaven.compile(config))
 }
