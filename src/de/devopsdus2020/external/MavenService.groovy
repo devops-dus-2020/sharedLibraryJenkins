cc3d017a46d1489106b3f424d16dbca57291625f
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-14 13:59:38 +0200

    allow config=null in mvn.groovy

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -20,6 +20,9 @@ class MavenService implements InterfaceMavenService {
     }
 
     String compile(Map config) {
+        if (!config) {
+            Map config [flag:"-f ", pompath: "${WORKSPACE}"]
+        }
         return executeMaven(config, "compile")
     }
 
