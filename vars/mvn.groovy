3861f85d992a880d8d9f949933aae9ebf8bf7139
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-14 14:43:28 +0200

    remove initiateMaven from vars/mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -2,26 +2,25 @@ import de.devopsdus2020.maven.*
 import de.devopsdus2020.external.*
 import groovy.util.logging.*
 
-Maven initiateMaven() {
+// def compile() {
+//     Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
+//     compile(config)
+// }
+
+def compile() {
     Closure logger = {String message -> println message}
     MavenService service = new MavenService(logger)
     Maven myMaven = new Maven(service)
-    return myMaven
-}
-
-def compile() {
     Map config = [flag:" -f ", pomfile: "${WORKSPACE}"]
-    Maven myMaven = initiateMaven()  
-
+    
     logger(myMaven.compile(config))
 }
 
 def compile(Map config) {
-    // Closure logger = {String message -> println message}
-    // MavenService service = new MavenService(logger)
-    // Maven myMaven = new Maven(service)
-    Maven myMaven = initiateMaven()
-    
+    Closure logger = {String message -> println message}
+    MavenService service = new MavenService(logger)
+    Maven myMaven = new Maven(service)
+
     logger(myMaven.compile(config))
 }
 
