d821a6ba21ed10990efd1f68f967cf4be57d77a3
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-14 13:51:47 +0200

    use println in MavenService

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -15,7 +15,7 @@ class MavenService implements InterfaceMavenService {
     String executeMaven(Map config, String phase) {
         def convertToValueString = {it.collect { /$it.value/ } join ""}
         def csequence = "mvn " + convertToValueString(config) + " " + phase
-        echo csequence
+        println csequence
         // return csequence.execute().text
     }
 
