c34fbb123623a35f9418f40c1b703f76c23a4e80
Mostafa <m.torbon@gmx.de>
2020-10-15 01:08:48 +0200

    Will left it with placeholder - have to think about other solution

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -39,6 +39,6 @@ class MavenService implements InterfaceMavenService {
     }
 
     String deploy(Map config) {
-        return this.executeMaven(config, "clean deploy --settings=/var/jenkins_home/workspace/settings.xml -DskipTests")
+        return this.executeMaven(config, "clean deploy --settings=${WORKSPACE}/settings.xml -DskipTests")
     }
 }
