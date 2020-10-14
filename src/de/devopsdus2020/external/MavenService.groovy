ddf630edb7ae6c8d33e399f41ed7cae794f201ca
Mostafa <m.torbon@gmx.de>
2020-10-15 01:02:12 +0200

    Small fix for path @ settings.xml

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -39,6 +39,6 @@ class MavenService implements InterfaceMavenService {
     }
 
     String deploy(Map config) {
-        return this.executeMaven(config, "clean deploy --settings=/home/aypahyo/settings.xml -DskipTests")
+        return this.executeMaven(config, "clean deploy --settings=/var/jenkins_home/settings.xml -DskipTests")
     }
 }
