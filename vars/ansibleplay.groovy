5116c057eca746be250f709656e3fb9c942f234b
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-15 20:28:21 +0200

    vars file editiert -> factory raus

--- vars/ansibleplay.groovy
+++ vars/ansibleplay.groovy
@@ -7,17 +7,13 @@ import groovy.transform.Field
 @Field final String config = "/var/jenkins_home/workspace/BBROW_Multi_master/buildImage.yml"
 
 
-//Factory pattern
-def makeMyAnsible(){
-        Closure logger = {String message -> println message}
-        AnsibleService service = new AnsibleService(logger)
-        Ansible myAnsible = new Ansible(service)
-        return myAnsible
-}
-
 
 //anhang ansible-playbook xxx.yml ...
 //credentialString = "-e USERNAME=${USERNAME} PASSWORD=${PASSWORD}"
 def imagebuild() { 
-    logger(makeMyAnsible().imagebuild(config))
+    Closure logger = {String message -> println message}
+    AnsibleService service = new AnsibleService(logger)
+    Ansible myAnsible = new Ansible(service)
+
+    myAnsible.imagebuild(config)
 }
