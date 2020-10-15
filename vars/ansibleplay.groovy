2f935d30157fe712ba93968d119054e7f4210ba7
Lysori <67781490+Lysori@users.noreply.github.com>
2020-10-15 21:37:13 +0200

    debugging 5

--- vars/ansibleplay.groovy
+++ vars/ansibleplay.groovy
@@ -3,12 +3,6 @@ import de.devopsdus2020.external.*
 import groovy.util.logging.*
 import groovy.transform.Field
 
-
-
-
-
-//anhang ansible-playbook xxx.yml ...
-//credentialString = "-e USERNAME=${USERNAME} PASSWORD=${PASSWORD}"
 def buildImage() { 
     Closure logger = {String message -> println message}
     AnsibleService service = new AnsibleService(logger)
