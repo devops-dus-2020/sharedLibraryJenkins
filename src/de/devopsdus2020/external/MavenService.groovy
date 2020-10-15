bed4cc43aa9e2516702084c6d36b1e4ab7318d66
Mostafa <m.torbon@gmx.de>
2020-10-15 09:55:53 +0200

    Readded WORKSPACE & NEXUS in vars

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -39,6 +39,6 @@ class MavenService implements InterfaceMavenService {
     }
 
     String deploy(Map config) {
-        return this.executeMaven(config, "clean deploy  -DskipTests")
+        return this.executeMaven(config, "clean deploy --settings=${WORKSPACE}/settings.xml -DskipTests")
     }
 }
