c20836543aaa0f745f149212bcd7bcb18022e9a0
Mostafa <m.torbon@gmx.de>
2020-10-18 11:46:45 +0200

    Trying it with deploy instead of redeploy

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -70,7 +70,7 @@ class MavenService implements InterfaceMavenService {
     }
 
     Integer tomcat(Map config) {
-        String phase = "tomcat7:redeploy -gs " + config.getAt("workspace") + " -DskipTests"
+        String phase = "tomcat7:deploy -gs " + config.getAt("workspace") + " -DskipTests"
         return this.executeMaven(config,  phase)
     }
 }
--- vars/config.groovy
+++ vars/config.groovy
@@ -2,10 +2,10 @@ import de.devopsdus2020.external.*
 
 def execute() {
     this.steps.configFileProvider(
-            bindings: [
+            bindings: [[
                 this.steps.configFile(
                     fileId: 'default',
                     variable: 'MAVEN_GLOBAL_SETTINGS')
-            ]) {
+            ]]) {
         }
     }
