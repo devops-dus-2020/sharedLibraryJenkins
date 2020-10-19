2dccb948ce204a61e24160c0d311bfb34ee507b6
melchma <67784962+melchma@users.noreply.github.com>
2020-10-19 14:56:58 +0200

    add imagepull to interfaceAnsibleService

--- src/de/devopsdus2020/ansible/InterfaceAnsibleService.groovy
+++ src/de/devopsdus2020/ansible/InterfaceAnsibleService.groovy
@@ -1,6 +1,7 @@
 package de.devopsdus2020.ansible
 
 interface InterfaceAnsibleService extends Serializable {
-    Integer imagebuild(Map configbuild)
-    Integer imagepush(Map configpush)
+    Integer imagebuild(Map config)
+    Integer imagepush(Map config)
+    Integer imagepull(Map config)
 }
