2c752fa438013a53fabfccc575770432e7478f0e
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-20 13:23:23 +0200

    implement exit code for mvn.compile()

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -17,19 +17,9 @@ def version() {
 }
 
 def compile() {
-    def exitvalue = makeMyMaven().compile(config)
-    echo "mvn.compile(): exit value = ${exitvalue}"
-    if (exitvalue != 0) {
-        error "Compile step failed!"
-    }
-}
-
-def failedcompile() {
-    config.mvn_args = ""
-    def exitvalue = makeMyMaven().compile(config)
-    echo "mvn.compile(): exit value = ${exitvalue}"
-    if (exitvalue != 0) {
-        error "Compile step failed!"
+    def exitcode = makeMyMaven().compile(config)
+    if (exitcode != 0) {
+        error "Compile step failed! Exit code: ${exitcode}"
     }
 }
 
