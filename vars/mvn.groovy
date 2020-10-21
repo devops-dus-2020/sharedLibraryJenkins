eb9c4be2c0b1f8062792883ed4d5241b0c5f8e28
WiggleThb <67831377+WiggleThb@users.noreply.github.com>
2020-10-21 08:21:24 +0200

    change logger back to String and add toString() in deploy(settings)

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -6,7 +6,7 @@ import groovy.transform.Field
 @Field final Map config = [mvn_args: "-f ${WORKSPACE}"]
 
 def makeMyMaven(){
-    Closure logger = {message -> println message.toString()}
+    Closure logger = {String message -> println message}
     MavenService service = new MavenService(logger)
     Maven myMaven = new Maven(service)
     return myMaven
